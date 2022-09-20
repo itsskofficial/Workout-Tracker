5b224fe25018568a76fff6b03b973ae9f055c0de
SK <sarthakkarandikar03@gmail.com>
2022-09-20 05:44:08 +0530

    added sheety auith

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import datetime
 WORKOUT_ID = "5ea00b72"
 WORKOUT_KEY= "dc2ad9c047d8e54ccc01842889623cc7"
 WORKOUT_END="https://trackapi.nutritionix.com/v2/natural/exercise"
+SHEET_KEY = "djsahendehidejnedje"
 SHEET_END= "https://api.sheety.co/8d8bd9e1daea2ae85b7b1cd31024a980/workoutTracker/workouts"
 
 #user input in simple language
@@ -30,6 +31,7 @@ print(workout_response.json())
 #adding row to sheet
 
 now=datetime.datetime.now()
-sheeet_params={"workout":{"date":now.strftime("%x"),"time":now.strftime("%X"),"exercise":workout_response.json()["exercises"][0]["name"],"duration":workout_response.json()["exercises"][0]["duration_min"],"calories":workout_response.json()["exercises"][0]["nf_calories"]}}
-sheet_response=requests.post(url=SHEET_END,json=sheeet_params)
+sheeet_params={"workout":{"date":now.strftime("%x"),"time":now.strftime("%X"),"exercise":workout_response.json()["exercises"][0]["name"].title(),"duration":workout_response.json()["exercises"][0]["duration_min"],"calories":workout_response.json()["exercises"][0]["nf_calories"]}}
+sheet_headers={"Authorization":f"Bearer {SHEET_KEY}"}
+sheet_response=requests.post(url=SHEET_END,json=sheeet_params,headers=sheet_headers)
 print(sheet_response.json())
